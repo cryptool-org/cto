3e2513d4c0f8456d6959c4947b0158293367b981
Florian Marchal <florian@marchal.de>
2019-06-23 21:11:00 +0200

    Fixed a few typos and other issues in the BARCODE generator implementation, and slightly refactored the code.

--- _ctoApps/codings/src/barcode/barcode.js
+++ _ctoApps/codings/src/barcode/barcode.js
@@ -1,34 +1,44 @@
 "use strict";
 
 window.addEventListener('load', function() {
-    @@include('../common/base.js')
-    @@include('./barcodeutilities.js')
-    @@include('./barcodegenerator.js')
-})
+    @@include('../common/base.js');
+    @@include('./barcodeutilities.js');
+    @@include('./barcodegenerator.js');
+});
+
+window.onload = function() {
+    BarcodeStandardChange();
+};
 
 function BarcodeStandardChange() {
     var barcodeStandard = document.getElementById("BarcodeStandard").value;
     if(BarcodeStandardSet(barcodeStandard) === false) {
-        BarcodeDetailsSet("The chosen barcode standard is invalid.");
+        BarcodeDetailsSet("${{ barcode.DETAILS_INVALID }}$");
     }
     else {
         if(barcodeStandard === "CODABAR") {
             BarcodeDetailsSet("${{ barcode.DETAILS_CODABAR }}$");
+            BarcodeDataSet("123");
         }
         else if(barcodeStandard === "CODE128") {
             BarcodeDetailsSet("${{ barcode.DETAILS_CODE128 }}$");
+            BarcodeDataSet("123ABCabc");
         }
         else if(barcodeStandard === "CODE39") {
             BarcodeDetailsSet("${{ barcode.DETAILS_CODE39 }}$");
+            BarcodeDataSet("123ABC");
         }
         else if(barcodeStandard === "EAN13") {
             BarcodeDetailsSet("${{ barcode.DETAILS_EAN13 }}$");
+            BarcodeDataSet("123456123456");
         }
         else if(barcodeStandard === "EAN8") {
             BarcodeDetailsSet("${{ barcode.DETAILS_EAN8 }}$");
+            BarcodeDataSet("1234567");
         }
         else if(barcodeStandard === "UPC") {
             BarcodeDetailsSet("${{ barcode.DETAILS_UPC }}$");
+            BarcodeDataSet("01234512345");
         }
         else {
             BarcodeDetailsSet(barcodeStandard);
@@ -42,6 +52,11 @@ function BarcodeDataChange() {
     barcodeResult.innerHTML = "<font color='#000000'>${{ barcode.CONTENT_RESULT_DEFAULT }}$</font>"
 }
 
+function BarcodeDataSet(_data) {
+    var barcodeData = document.getElementById("BarcodeData");
+    barcodeData.value = _data;
+}
+
 function BarcodeGenerate() {
     var barcodeStandard = document.getElementById("BarcodeStandard").value;
     var barcodeData = document.getElementById("BarcodeData").value;
@@ -49,7 +64,8 @@ function BarcodeGenerate() {
 }
  
 function BarcodeDetailsSet(_details) {
-    document.getElementById("BarcodeDetails").innerHTML = _details;
+    var barcodeDetails = document.getElementById("BarcodeDetails");
+    barcodeDetails.innerHTML = _details;
 }
 
 function BarcodeImageValid(_valid) {
