b2029d2a8fe4f76fcc6357122ad47b8e32f2c9d7
rech <sven.rech@cryptool.org>
2020-03-28 01:18:06 +0100

    Factorization msieve component: Fixed unit test by making worker script path parameterizable.

--- _ctoApps/factorization/src/msieve/msieve.js
+++ _ctoApps/factorization/src/msieve/msieve.js
@@ -2,7 +2,7 @@
 
 window.addEventListener('load', function() {
     @@include('../../node_modules/expr-eval/dist/bundle.min.js')
-    @@include('../common/msieve.js')
+    @@include('../common/msieve.js', { "msieveWorkerPath": "/_ctoApps/msieve/worker/msieveWorker.js" })
     @@include('../common/factorizer.js')
     @@include('../common/base.js')
 })
